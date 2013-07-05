805ed5df55685d0935326762e8422ea11565a101
Markus Mayer <development@widemeadows.de>
2013-07-05 23:47:45 +0200

    simple statistics

--- mas02/Application.cpp
+++ mas02/Application.cpp
@@ -14,6 +14,7 @@
 #include "Application.h"
 
 using namespace std;
+using namespace envi;
 
 /// <summary>
 /// Initializes a new instance of the <see cref="Application"/> class.
@@ -50,12 +51,14 @@ void Application::createWindow(const string& name)
 /// Converts an ENVI image to OpenCV
 /// </summary>
 /// <param name="image">The image.</param>
+/// <param name="samples">The number of samples.</param>
+/// <param name="lines">The number of lines.</param>
+/// <param name="bands">The number of bands.</param>
 /// <returns>The converted image</returns>
-IplImagePtr Application::enviToOpenCv(const envi::image_t& image, const envi::samplecount_t& samples, const envi::linecount_t& lines, const envi::bandcount_t& bands) const
+IplImagePtr Application::enviToOpenCv(const image_t& image, const samplecount_t& samples, const linecount_t& lines, const bandcount_t& bands) const
 {
     assert(bands == 1);
-    
-    
+   
     IplImagePtr displayImage(cvCreateImage(cvSize(samples, lines), IPL_DEPTH_8U, bands));
 
     typedef int_fast32_t omp_linecount_t; // OpenMP needs signed integral type
@@ -64,16 +67,16 @@ IplImagePtr Application::enviToOpenCv(const envi::image_t& image, const envi::sa
     #pragma omp parallel for
     for(omp_linecount_t y=0; y<omp_lines; ++y)
     {
-        envi::line_t& line = image[y];
+        line_t& line = image[y];
         uint_fast32_t lineOffset = y*samples;
 
         // TODO: when multiple bands are needed, implement another loop or specific behaviour for regular band counts (1, 3, 4)
-        for(envi::samplecount_t x=0; x<samples; ++x)
+        for(samplecount_t x=0; x<samples; ++x)
         {
             char& pixel = displayImage->imageData[lineOffset+x];
 
             // pick the sample and convert it to 8-bit unsigned
-            envi::sample_t sample = line[x];
+            sample_t sample = line[x];
             uint_fast8_t value = static_cast<uint_fast8_t>(sample);
 
             // assign sample (prediction friendly)
@@ -91,6 +94,89 @@ IplImagePtr Application::enviToOpenCv(const envi::image_t& image, const envi::sa
     return displayImage;
 }
 
+
+/// <summary>
+/// Naive calculation of the statistics
+/// </summary>
+/// <param name="image">The image.</param>
+/// <param name="samples">The number of samples.</param>
+/// <param name="lines">The number of lines.</param>
+/// <param name="bands">The number of bands.</param>
+/// <param name="min">Output: The minimum value.</param>
+/// <param name="max">Output: The maximum value.</param>
+/// <param name="mean">Output: The mean value.</param>
+/// <param name="stdDev">Output: The standard deviation.</param>
+void Application::calculateStatisticsNaive(const image_t& image, const samplecount_t& samples, const linecount_t& lines, const bandcount_t& bands, 
+                                            out float& min, out float& max, out float& mean, out float& stdDev) const
+{
+    assert(bands == 1);
+
+    min = FLT_MAX;
+    max = FLT_MIN;
+    mean = 0;
+    stdDev = 0;
+
+    const float invLines = 1.0F / lines;
+    const float invSamples = 1.0F / samples;
+    const float invSamplesA = 1.0F / (samples-1);
+
+    // first run: gather min, max and mean
+    for(linecount_t y=0; y<lines; ++y)
+    {
+        const line_t& line = image[y];
+        float lineSum = 0;
+
+        // TODO: when multiple bands are needed, implement another loop or specific behaviour for regular band counts (1, 3, 4)
+        for(samplecount_t x=0; x<samples; ++x)
+        {
+            const sample_t& sample = line[x];
+            
+            // update mean
+            lineSum += sample;
+
+            // update minimum
+            if (sample < min) {
+                min = sample;
+            }
+
+            // update maximum
+            if (sample > max) {
+                max = sample;
+            }
+        }
+
+        // aggregate mean per line
+        mean += lineSum * invSamples;
+    }
+
+    // adjust mean over all lines
+    mean *= invLines;
+    
+    // second run: calculate variance
+    float variance = 0;
+    for(linecount_t y=0; y<lines; ++y)
+    {
+        const line_t& line = image[y];
+        float rowVariance = 0;
+
+        // TODO: when multiple bands are needed, implement another loop or specific behaviour for regular band counts (1, 3, 4)
+        for(samplecount_t x=0; x<samples; ++x)
+        {
+            const sample_t& sample = line[x];
+            
+            const float diff = sample - mean;
+            rowVariance += (diff * diff);
+        }
+
+        // aggregate variance per line
+        variance += rowVariance * invSamplesA; // Stichprobenvarianz, sample variance
+    }
+
+    // second run, part two: scale variance and calculate standard deviation
+    variance *= invLines;
+    stdDev = sqrt(variance);
+}
+
 /// <summary>
 /// Runs this instance.
 /// </summary>
@@ -103,17 +189,24 @@ void Application::run()
     if (!inputFile.is_open()) throw runtime_error("Could not open input file");
 
     // load the image data
-    const envi::samplecount_t samples = 5000;
-    const envi::linecount_t lines = 2000;
-    const envi::bandcount_t bands = 1;
-    envi::ENVIFileReader reader(samples, lines, bands);
+    const samplecount_t samples = 5000;
+    const linecount_t lines = 2000;
+    const bandcount_t bands = 1;
+    ENVIFileReader reader(samples, lines, bands);
 
     cout << "Loading image ... ";
-    envi::image_t image = reader.read(inputFile);
+    image_t image = reader.read(inputFile);
     cout << "done." << endl;
 
     // close the input file
     inputFile.close();
+
+    // calculate naive statistics
+    float min, max, mean, stddev;
+    cout << "Calculating statistics (naive) ... ";
+    calculateStatisticsNaive(image, samples, lines, bands, min, max, mean, stddev);
+    cout << "done" << endl;
+    cout << "Range " << min << " .. " << max << ", mean " << mean << " +/- " << stddev << endl;
 
     // convert image to OpenCV image.
     cout << "Converting image for display ... ";
--- mas02/Application.h
+++ mas02/Application.h
@@ -9,6 +9,9 @@
 #include "ENVIFileReader.h"
 #include "OpenCvImage.h"
 
+/// <summary>Marks a variable as output</summary>
+#define out
+
 /// <summary>
 /// Main application class
 /// </summary>
@@ -36,6 +39,8 @@ public:
     /// </summary>
     void run() throw(std::runtime_error);
 
+private:
+
     /// <summary>   Creates a window. </summary>
     ///
     /// <remarks>   Markus, 05.07.2013. </remarks>
@@ -53,13 +58,29 @@ public:
     /// <param name="name"> [in] The name. </param>
     void createWindow(const std::string& name);
 
-private:
     /// <summary>
     /// Converts an ENVI image to OpenCV
     /// </summary>
     /// <param name="image">The image.</param>
+    /// <param name="samples">The number of samples.</param>
+    /// <param name="lines">The number of lines.</param>
+    /// <param name="bands">The number of bands.</param>
     /// <returns>The converted image</returns>
     IplImagePtr enviToOpenCv(const envi::image_t& image, const envi::samplecount_t& samples, const envi::linecount_t& lines, const envi::bandcount_t& bands) const;
+
+    /// <summary>
+    /// Naive calculation of the statistics
+    /// </summary>
+    /// <param name="image">The image.</param>
+    /// <param name="samples">The number of samples.</param>
+    /// <param name="lines">The number of lines.</param>
+    /// <param name="bands">The number of bands.</param>
+    /// <param name="min">Output: The minimum value.</param>
+    /// <param name="max">Output: The maximum value.</param>
+    /// <param name="mean">Output: The mean value.</param>
+    /// <param name="stdDev">Output: The standard deviation.</param>
+    void calculateStatisticsNaive(const envi::image_t& image, const envi::samplecount_t& samples, const envi::linecount_t& lines, const envi::bandcount_t& bands, 
+                                  out float& min, out float& max, out float& mean, out float& stdDev) const;
 };
 
 #endif
